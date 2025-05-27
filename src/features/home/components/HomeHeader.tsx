480808a4e18bde19c01cbe4a8b09b2010f77cc5d
leonardo martins medeiros <leonardo.martins.medeiros@live.com>
2025-05-26 21:59:42 -0300

    ajustes visuais

--- src/features/home/components/HomeHeader.tsx
+++ src/features/home/components/HomeHeader.tsx
@@ -7,7 +7,7 @@ export default function HomeHeader() {
   const router = useRouter();
 
   return (
-    <header className="flex flex-wrap items-center justify-between bg-white px-4 py-4 shadow-sm sm:px-6 md:px-8">
+    <header className="flex flex-wrap items-center justify-between bg-black px-4 py-4 shadow-sm sm:px-6 md:px-8">
       <div className="flex">
         <div className="relative h-5 w-6">
           <Image
@@ -18,29 +18,29 @@ export default function HomeHeader() {
             priority={false}
           />
         </div>
-        <div className="text-brand-primary text-xl font-bold italic">
+        <div className="text-brand-secondary text-xl font-bold italic">
           Bytebank
         </div>
       </div>
       {
         //<div className="text-brand-primary text-xl font-bold italic">Bytebank</div>
       }
-      <nav className="hidden gap-6 text-sm font-medium text-neutral-700 md:flex">
+      <nav className="text-brand-secondary hidden gap-6 text-sm font-medium md:flex">
         <a href="#">{HEADER_CONSTANTS.about}</a>
         <a href="#">{HEADER_CONSTANTS.services}</a>
       </nav>
       <div className="mt-4 flex w-full gap-2 md:mt-0 md:w-auto md:justify-end">
         <button
-          className="border-brand-primary text-brand-primary rounded-md border px-4 py-1 text-sm"
-          onClick={() => router.push(ROUTE_CONSTANTS.signin)}
-        >
-          {HEADER_CONSTANTS.account}
-        </button>
-        <button
-          className="bg-brand-primary rounded-md px-4 py-1 text-sm text-white"
+          className="bg-brand-secondary rounded-md px-4 py-1 text-sm text-white"
           onClick={() => router.push(ROUTE_CONSTANTS.signup)}
         >
           {HEADER_CONSTANTS.openAccount}
+        </button>
+        <button
+          className="border-brand-secondary text-brand-secondary rounded-md border px-4 py-1 text-sm"
+          onClick={() => router.push(ROUTE_CONSTANTS.signin)}
+        >
+          {HEADER_CONSTANTS.account}
         </button>
       </div>
     </header>
