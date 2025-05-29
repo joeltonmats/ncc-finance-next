c0230d5dee95492efa4552d55b9e7e65f8f4c835
Carlos Ferreira <carlosrfjr.dev@gmail.com>
2025-05-29 00:55:49 -0300

    feat(balance) add subtract balance route

--- src/app/api/back/balance/[balanceId]/subtract/route.ts
+++ src/app/api/back/balance/[balanceId]/subtract/route.ts
@@ -1,15 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { subtractBalance } from "@/service/balanceService";
 
-/* example: /api/debug/balance/subtract?id=USER_ID&amount=50 */
-export async function PUT(req: NextRequest) {
-  const { id, amount } = await req.json();
-  if (!id || typeof amount !== "number") {
+/* example: /api/back/balance/USER_ID/subtract */
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ balanceId: string }> }
+) {
+  const { balanceId } = await params;
+  if (!balanceId) {
+    return NextResponse.json({ error: "Missing balance ID" }, { status: 400 });
+  }
+  const { amount } = await req.json();
+  if (typeof amount !== "number") {
     return NextResponse.json(
       { error: "Missing id or amount" },
       { status: 400 }
     );
   }
-  const result = await subtractBalance(id, amount);
+  const result = await subtractBalance(balanceId, amount);
   return NextResponse.json(result);
 }
